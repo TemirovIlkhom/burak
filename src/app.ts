e0e0c6fa5e7d866b7183657f17bd6dfe58657aae
temirovilkhom <ilhomtemirov9601@gmail.com>
2024-06-09 15:31:40 +0900

    fix: modif system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import router from "./router";
+import routerAdmin from "./routerAdmin";
 
 /** 1-ENTRANCE **/
 const app = express();
@@ -17,5 +18,6 @@ app.set("views", path.join(__dirname, "views"));
 app.set("views engine", "ejs");
 
 /** 4-ROUTERS **/
+app.use("/admin", routerAdmin); // SSR EJS
 app.use("/", router);   // Middleware Design Pattern
 export default app;
