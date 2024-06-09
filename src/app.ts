df672af321157633ba1ed6567e40c4d3a388bbcd
temirovilkhom <ilhomtemirov9601@gmail.com>
2024-06-09 15:07:29 +0900

    feat: create routing and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import path from "path";
+import router from "./router";
 
 /** 1-ENTRANCE **/
 const app = express();
 // console.log("__dirname:", __dirname);
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"))); //Design Pattern
 app.use(express.urlencoded({extended: true}));
 app.use(express.json()); 
 
@@ -16,5 +17,5 @@ app.set("views", path.join(__dirname, "views"));
 app.set("views engine", "ejs");
 
 /** 4-ROUTERS **/
-
+app.use("/", router);   // Middleware Design Pattern
 export default app;
