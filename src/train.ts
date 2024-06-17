f91ba8fb85ba45a8bca84f81d976e928e86ce6f9
temirovilkhom <ilhomtemirov9601@gmail.com>
2024-06-17 16:49:10 +0900

    feat: develop restaurant login process

--- src/train.ts
+++ src/train.ts
@@ -1,17 +1,20 @@
 // H2 - TASK:
 
-function digits(input: string): string {
-  let digits = ""; // Raqamlarni saqlash uchun bo'sh string yaratamiz
+// function digits(input: string): string {
+//   let digits = ""; // Raqamlarni saqlash uchun bo'sh string yaratamiz
 
-  // Har bir belgini tekshirish uchun for tsiklini ishlatamiz
-  for (let i = 0; i < input.length; i++) {
-      const char = input[i];
-      if (char >= '0' && char <= '9') {
-          digits += char; // Agar belgi raqam bo'lsa, uni digits stringiga qo'shamiz
-      }
-  }
+//   // Har bir belgini tekshirish uchun for tsiklini ishlatamiz
+//   for (let i = 0; i < input.length; i++) {
+//       const char = input[i];
+//       if (char >= '0' && char <= '9') {
+//           digits += char; // Agar belgi raqam bo'lsa, uni digits stringiga qo'shamiz
+//       }
+//   }
 
-  return digits; // Faqat raqamlardan iborat stringni qaytaramiz
-}
+//   return digits; // Faqat raqamlardan iborat stringni qaytaramiz
+// }
 
-console.log(digits("m1fdg4gi15t"));
+// console.log(digits("m1fdg4gi15t"));
+
+
+
