a22cc445a670168f9b60de363e6ccdf703f4a95d
temirovilkhom <ilhomtemirov9601@gmail.com>
2024-07-23 19:36:14 +0900

    feat: develop getTopUsers rest Apt

--- src/router.ts
+++ src/router.ts
@@ -16,13 +16,13 @@ router.get(
     memberController.verifyAuth,
     memberController.getMemberDetail
 );
-
 router.post(
     "/member/update", 
     memberController.verifyAuth,
     uploader("members").single("memberImage"),
     memberController.updateMember
 );
+router.get("/member/top-users", memberController.getTopUsers);
 
 /** Product */
 
