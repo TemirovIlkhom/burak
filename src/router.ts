16dea160ffa850db555e1ab247cea0214591b40c
temirovilkhom <ilhomtemirov9601@gmail.com>
2024-07-23 15:40:43 +0900

    feat: develop logout process

--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,7 @@ import memberController from "./controllers/member.controller";
 /** Member */
 router.post("/member/login", memberController.login); // REST API
 router.post("/member/signup", memberController.signup);
+router.post("/member/logout", memberController.verifyAuth);
 router.get("/member/detail", memberController.verifyAuth);
 
 /** Product */
