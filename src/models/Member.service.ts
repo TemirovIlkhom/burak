f604df7a1f5556a80a76a423fc13661d4e6d0bd8
temirovilkhom <ilhomtemirov9601@gmail.com>
2024-07-23 19:56:21 +0900

    feat: create getRestaurant rest Api

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -13,6 +13,19 @@ class MemberService {
     }
 
     //** SPA */
+
+    public async getRestaurant(): Promise<Member> {
+      const result = await this.memberModel
+      .findOne({ memberType: MemberType.RESTAURANT })
+      .lean()
+      .exec();
+
+      if(!result)  
+        throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+      
+      return result;
+  }
+
     public async signup(input: MemberInput): Promise<Member> {
         const salt = await bcrypt.genSalt();
         input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
@@ -25,7 +38,7 @@ class MemberService {
             console.error("Error, model:signup", err);
             throw new Errors(HttpCode.BAD_REQUEST, Message.USED_NICK_PHONE);
         }           
-    }
+    };
 
     public async login(input: LoginInput): Promise<Member> {
         // TODO: Consider member status later 
